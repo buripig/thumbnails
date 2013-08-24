717c9eb020a8e40475e48b7da4b48ec183539d71
buripig <buripig@gmail.com>
2013-08-24 19:14:56 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thumbnails::Application.config.secret_token = '10c0347840baf5042304c5e8173ebcd6f0bcede07bb48339d9778529252715297d840f990792ab464ea5c4580ff7b4d78df59157459952b382e5ca7e44c5da3c'
