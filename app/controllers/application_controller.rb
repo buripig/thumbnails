aec640d50b9ab3f4d5444d5ee3c02583c4db4086
buripig <buripig@gmail.com>
2013-08-25 01:45:08 +0900

    TOPページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
+  include CommonController
+  
   protect_from_forgery
+  
+  rescue_from Exception, with: :common_rescue
+  
+  private
+  
+  def menu_token
+    params[:controller]
+  end
+  helper_method :menu_token
+  
+  def sub_menu_token
+    params[:action]
+  end
+  helper_method :sub_menu_token
 end
